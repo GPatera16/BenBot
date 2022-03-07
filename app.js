e73e6bf0d13a15e4b1266bd6aa196c24334f5d59
lelouch <gpatera16@gmail.com>
2022-03-08 00:31:58 +0100

    fixata roba parte 999

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const {Telegraf} = require('telegraf');
 
 const bot = new Telegraf('5164325716:AAGi22CABMitcO3HaG7HI_5wCZIyQ8_nY-g');
 
+let contatore = 0;
+
 bot.command('negozio',ctx =>{
     let sussyd=ctx.chat.id;
     bot.telegram.sendMessage(ctx.chat.id, 'Benvenuto al negozio di Giovanni Rana, Cosa le posso offrire?',
@@ -17,14 +19,20 @@ bot.command('negozio',ctx =>{
         }
     })
 })
+bot.command('contatore',ctx =>{
+    let sussyd=ctx.chat.id;
+    bot.telegram.sendMessage(ctx.chat.id, 'Hai ottenuto un mio prodotto',contatore, ' volte')
+})
 
 bot.action('ravioli', ctx =>{
+    contatore=contatore+1;
     bot.telegram.sendMessage(ctx.chat.id,'Ecco a lei')
     bot.telegram.sendPhoto(ctx.chat.id,{
         source:"res/ravioli.png"
     })
 })
 bot.action('tortellini', ctx =>{
+    contatore=contatore+1;
     bot.telegram.sendMessage(ctx.chat.id,'Ecco a lei')
     bot.telegram.sendPhoto(ctx.chat.id,{
         source:"res/tortellini.jpg"
